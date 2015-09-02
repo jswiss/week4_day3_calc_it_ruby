4eedccb727c41f8f646cb43e4a684d56b82fb71b
Joshua Swiss <swissjoshua@gmail.com>
2015-09-02 21:04:30 +0100

    bmi and distance working

--- app.rb
+++ app.rb
@@ -7,17 +7,33 @@ get '/' do
   erb :home
 end
 
-post '/calculate' do
+post '/basic_calc' do
   case params[:symbol]
   when '+' then @basic = params[:input1].to_f + params[:input2].to_f
   when '-' then @basic = params[:input1].to_f - params[:input2].to_f
   when '*' then @basic = params[:input1].to_f * params[:input2].to_f
   when '/' then @basic = params[:input1].to_f / params[:input2].to_f
   else
-    erb :basic
+    erb :basic_calc
   end
-    erb :basic
+    erb :basic_calc
+end
 
+post '/bmi_calc' do
+    case params[:unit].downcase
+    when 'metric' then @bmi = params[:weight].to_f / (params[:height].to_f * params[:height].to_f)
+    when 'imperial' then @bmi = params[:weight].to_f / (params[:height].to_f * params[:height].to_f) * 703
+    else
+    erb :bmi_calc
+  end
+  erb :bmi_calc
+end
+
+post '/dist_calc' do
+  @mpg = (params[:mpg].to_f > 60) ? [0, params[:mpg].to_f - (params[:speed].to_f - 60) * 2].max : params[:mpg].to_f 
+  @cost = params[:distance].to_f/(@mpg/params[:cost].to_f)
+  @time = params[:distance].to_f/params[:mph].to_f
+  erb :distance 
 end
 
 get '/basic' do
