c0f8d0f680a4fc991d983fc852de0667c1f7b43d
Joshua Swiss <swissjoshua@gmail.com>
2015-09-02 18:19:10 +0100

    layout and app basic menu

---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry'
+
+get '/' do
+  @title = 'Home'
+  erb :home
+end
+
+get '/basic' do
+  @title = 'Basic Calculator'
+  erb :basic
+end
+
+get '/mortgage' do
+  @title = 'Mortgage Calculator'
+  erb :mortgage
+end
+
+get '/bmi' do
+  @title = 'Body Mass Index Calculator'
+  erb :bmi
+end
+
+get '/distance' do
+  @title = 'Distance Calculator'
+  erb :distance
+end
+
+get '/exchange' do
+  @title = 'Exchange Rate Calculator'
+  erb :exchange
+end
+
